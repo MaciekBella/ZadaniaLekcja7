a8af0cafc837463812634b51d9c69813413b9e18
Maciek <maciejbella@op.pl>
2022-08-28 15:52:26 +0200

    Dodano zadanie 5

---
+++ src/main/java/com/example/zad5/Main.java
@@ -0,0 +1,10 @@
+package com.example.zad5;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean raw = RectangularTriangle.getCheckRectangularTriangle(6,8,10);
+
+    }
+
+}
