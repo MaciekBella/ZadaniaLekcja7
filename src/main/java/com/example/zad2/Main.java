5ab6dd2bbef0dc5cd4c001f4ca9efaf90d2cd162
Maciek <maciejbella@op.pl>
2022-08-28 15:47:50 +0200

    Dodano zadanie 2

---
+++ src/main/java/com/example/zad2/Main.java
@@ -0,0 +1,9 @@
+package com.example.zad2;
+
+public class Main {
+    public static void main(String[] args){
+        Movie movie = new Movie();
+        System.out.println(movie.Movie());
+
+    }
+}
